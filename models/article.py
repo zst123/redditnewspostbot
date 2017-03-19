dfcada67c7febbff4648617b838e412f57e3937d
Amos Ng <amosng1@gmail.com>
2017-03-20 00:07:16 +0800

    Parse links and images separately
    Continuation of 508fdd88fa21573163f140932fe0e55158b92745, this commit aims to try and parse <img> and <a> tags separately in source, instead of using regexp to do it.

--- models/article.py
+++ models/article.py
@@ -60,12 +60,16 @@ class Article():
 
         # Modify specified selectors
         article_html = self.modify_selectors(
-            article_soup, settings["modifications"]).prettify()
+            article_soup, settings["modifications"])
 
         # Replace local regex
         article_html = self.replace_local(
             article_html, settings["replacements"])
         self.article_html = article_html
+
+        # Replace "img" and "a" with markdown
+        article_soup = BeautifulSoup(article_html, "html.parser")
+        article_html = self.replace_markdown(article_soup)
 
         # Replace global regex
         article_text = BeautifulSoup(article_html, "html.parser").text
@@ -84,7 +88,77 @@ class Article():
                     selected.decompose()
                 if action == "unwrap":
                     selected.unwrap()
-        return soup
+        return soup.prettify()
+
+    def replace_markdown_a(self, a):
+        a_href = "#"
+        a_desc = ": "
+        replacement = ""
+
+        # Try checking if link contains an image
+        if a.img:
+            replacement += self.replace_markdown_img(
+                a.img, linked=True) + "\n\n"
+            a_desc = " OF IMAGE ABOVE"
+        else:
+            try:
+                a_desc += a.text.strip()
+            except:
+                raise
+
+        # Try replacing link meta
+        try:
+            a_href = a["href"]
+            if a_href.startswith("/"):
+                a_href = self.domain + a_href
+            elif not (a_href.startswith("http")
+                      or a_href.startswith("ftp")):
+                a_href = self.domain + "/" + a_href
+        except:
+            raise
+
+        replacement += "[LINK%s](%s)\n\n" % (a_desc, a_href)
+        return replacement
+
+    def replace_markdown_img(self, img, linked=False):
+        img_src = "#"
+        img_alt = ": "
+
+        # Try parsing src of img
+        try:
+            img_src = img["src"]
+            if img_src.startswith("/"):
+                img_src = self.domain + img_src
+            elif not (img_src.startswith("http")
+                      or img_src.startswith("ftp")):
+                img_src = self.domain + "/" + img_src
+        except (KeyError, TypeError):
+            return ""
+
+        # Try getting img alt else default empty
+        try:
+            img_alt = img["alt"]
+        except KeyError:
+            img_alt = ""
+
+        # Strip img alt
+        img_alt = img_alt.strip()
+
+        replacement = "[IMAGE%s](%s)\n\n" % (img_alt, img_src)
+        return replacement
+
+    def replace_markdown(self, soup):
+        # Parse links to markdown
+        for a in soup("a"):
+            replacement = self.replace_markdown_a(a)
+            a.replace_with(replacement)
+
+        # Parse images to markdown
+        for img in soup("img"):
+            replacement = self.replace_markdown_img(img)
+            img.replace_with(replacement)
+
+        return soup.prettify()
 
     def replace_local(self, text, replacements):
         for find, replace in replacements.items():
