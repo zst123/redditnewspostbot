1b8dca8d5674c975098c1da3c68168de50cfc0ae
Amos Ng <amosng1@gmail.com>
2017-03-21 18:52:59 +0800

    Added an easier test script to test article parsing

---
+++ test_article.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+##
+# RedditNewsPostBot Article Tester
+# By Amos (LFlare) Ng
+##
+# Import dependencies
+import sys
+
+# Import Article model
+from models.article import Article
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        for url in sys.argv[1:]:
+            article_text = Article(url=url).get_article_text()
+            print(article_text)
